08b74ef17f593cc89b8fc5d62714a7843d182778
Wesley Spinks <wspinks@redhat.com>
2025-08-25 15:32:26 -0400

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     build: {
-            base: '/plots/',
+            base: '/',
             outDir: './docs', // relative to index.html
             assetsDir: './assets',
             watch: false,
