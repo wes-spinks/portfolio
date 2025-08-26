bb3e7497f7566a9672c9c393e621eb8b473eba86
Wesley Spinks <wspinks@redhat.com>
2025-08-26 12:39:08 -0400

    update img ref

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,43 +3,43 @@ export const workProjects = [
       title: "CI/CD Pipelines",
       description: "Custom SDLC pipelines, from linting, testing, and security scanning to release, rollbacks, and rollouts - using a variety of tools",
       technologies: ["Jenkins", "Ansible", "GitLab CI/CD", "Github", "Tekton"],
-      image: "/spinks.work/screens/gitlab_cicd.png"
+      image: "./screens/gitlab_cicd.png"
     },
     {
       title: "Authentication Proxy",
       description: "OIDC proxy service integrates Red Hat Composer AI Chatbot (customer demo) with enterprise SSO, protecting internal APIs and services. https://github.com/redhat-composer-ai/appOfApps/pull/24",
       technologies: ["Keycloak", "ArgoCD", "Helm", "OAuth2", "React", "Quarkus"],
-      image: "/spinks.work/screens/composer_oauth.png"
+      image: "./screens/composer_oauth.png"
     },
     {
       title: "Retrieval-augmented Generative AI",
       description: "Integrated vector search and LLMs to provide contextual answers to enterprise knowledge base queries.",
       technologies: ["LangChain", "Postgres", "OpenAI", "vLLM", "Streamlit", "GraphRAG"],
-      image: "/spinks.work/screens/shadowgpt-ui.png"
+      image: "./screens/shadowgpt-ui.png"
     },
     {
       title: "ETL and ELT Pipelines",
       description: "Secure data pipelines, including Snowflake data to vector store for HNSW knowledge graph search.",
       technologies: ["Astronomer", "Apache Airflow", "OpenShift", "Streamlit", "Jupyter", "Snowflake", "LlamaIndex", "Postgres"],
-      image: "/spinks.work/screens/airflow-dags.png"
+      image: "./screens/airflow-dags.png"
     },
     {
       title: "Model Inference Services",
       description: "Cloud-native LLM completion services using popular Python libraries",
       technologies: ["vLLM", "K8s", "Ollama", "llama-cpp", "AWS S3", "Minio", "InstructLab"],
-      image: "/spinks.work/screens/vLLM_startup.png"
+      image: "./screens/vLLM_startup.png"
     },
     {
       title: "Natural Language to SQL Agent",
       description: "NLP to SQL agent takes natural language chats and queries central data warehouse",
       technologies: ["Autogen", "LangGraph", "LangChain", "Snowflake"],
-      image: "/spinks.work/screens/nlp-sql-agent.png"
+      image: "./screens/nlp-sql-agent.png"
     },
     {
       title: "AI Code Assist",
       description: "Explored various code assist tools, providing feedback for wider enterprise consumption",
       technologies: ["MCP", "Google Gemini", "Claude Code", "Cursor", "Cline", "WatsonX", "Github Copilot"],
-      image: "/spinks.work/screens/ai-coding.png"
+      image: "./screens/ai-coding.png"
     },
   ]
   
@@ -48,13 +48,13 @@ export const workProjects = [
       title: "Godot Game Development",
       description: "Prototype server-client 3D game exploring collision meshes, textures, physics, and 3D modeling",
       technologies: ["GDScript", "Blender", ],
-      image: "/spinks.work/screens/godot-1.png"
+      image: "./screens/godot-1.png"
     },
     {
       title: "Clover-Google Integrations",
       description: "Assisting local small businesses with Clover inventory management APIs via Google Sheets and Google Apps Script",
       technologies: ["Clover API", "Google API", "GCP", "AppSheet"],
-      image: "/spinks.work/screens/clover-gas.png"
+      image: "./screens/clover-gas.png"
     },
   ]
   
